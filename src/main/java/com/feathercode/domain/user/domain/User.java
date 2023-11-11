a3cd76152c97a80bec452d05065b49678fd071b6
kangjjs <emdtod12@naver.com>
2023-11-11 09:15:27 +0900

    Feat:유저 엔티티 구현

---
+++ src/main/java/com/feathercode/domain/user/domain/User.java
@@ -0,0 +1,41 @@
+package com.feathercode.domain.user.domain;
+
+import com.feathercode.domain.user.model.Gender;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", length = 20)
+    @NotNull
+    private String username;
+
+    @Column(length = 12)
+    @NotNull
+    private String nickname;
+
+    @Column(unique = true)
+    @NotNull
+    private String email;
+
+    private String contents;
+    private String notionLink;
+    private String githubLink;
+    private String userImage;
+
+    @NotNull
+    private int likesCnt;
+    @NotNull
+    private int recommendationsCnt;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+}
---
+++ src/main/java/com/feathercode/domain/user/model/Gender.java
@@ -0,0 +1,16 @@
+package com.feathercode.domain.user.model;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Gender {
+
+    MAN("남자"),
+    WOMAN("여자");
+
+    @JsonValue
+    private final String name;
+}
