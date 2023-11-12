c5347bfeddf3833a9e9eba86902852ffb565f6ab
kangjjs <emdtod12@naver.com>
2023-11-12 12:02:02 +0900

    feat:User 관련 엔티티 구현

---
+++ src/main/java/com/feathercode/domain/user/domain/UserLike.java
@@ -0,0 +1,26 @@
+package com.feathercode.domain.user.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserLike {
+
+    @Id
+    @Column(name = "user_like_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "to_user_id")
+    private Long toUserId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "from_user_id")
+    private User user;
+}
