e1d05b1375f64f4682b6d2b45ed808ad6b0127ba
LSH <104637774+lsh2613@users.noreply.github.com>
2023-11-14 18:33:55 +0900

    feat: comment entity 구현

---
+++ src/main/java/com/feathercode/domain/community/entity/CommentLike.java
@@ -0,0 +1,21 @@
+package com.feathercode.domain.community.entity;
+
+import com.feathercode.domain.user.domain.User;
+import jakarta.persistence.*;
+
+@Entity
+public class CommentLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_like_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
